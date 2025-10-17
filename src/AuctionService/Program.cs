2cc41ae07a974a08b1beb960dccaed5de2d5018a
Steffen Grønmo <steffen.gronmo@arbeidstilsynet.no>
2025-10-17 11:36:33 +0200

    Dockerized the AuctionService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -33,6 +33,11 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMq:Username"] ?? "guest");
+            h.Password(builder.Configuration["RabbitMq:Password"] ?? "guest");
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
