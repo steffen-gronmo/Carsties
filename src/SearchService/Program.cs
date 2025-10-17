6ad2a864aeabba677162237b74525c575d811ebd
Steffen Grønmo <steffen.gronmo@arbeidstilsynet.no>
2025-10-17 11:46:59 +0200

    Dockerized the SearchService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -25,6 +25,11 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMq:Username"] ?? "guest");
+            h.Password(builder.Configuration["RabbitMq:Password"] ?? "guest");
+        });
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r => r.Interval(5, 5));
